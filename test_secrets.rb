dfd2555322d7fb06857c4993d18d91cc8a4b07a9
nikolai-markov-workato <121014301+nikolai-markov-workato@users.noreply.github.com>
2023-01-18 13:54:22 +0400

    test secrets

---
+++ test_secrets.rb
@@ -0,0 +1,7 @@
+puts "FOO_KEY.class: #{ENV['FOO_KEY'].class}"
+puts "FOO_KEY.size: #{ENV['FOO_KEY'].to_s.size}"
+puts "FOO_KEY.inspect: #{ENV['FOO_KEY'].inspect}"
+
+puts "REPO_KEY.class: #{ENV['REPO_KEY'].class}"
+puts "REPO_KEY.size: #{ENV['REPO_KEY'].to_s.size}"
+puts "REPO_KEY.inspect: #{ENV['REPO_KEY'].inspect}"
